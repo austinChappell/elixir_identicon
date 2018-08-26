f343d037cb4a6c58cdf1576670956c82c6c22d96
austinChappell <Austin.W.Chappell@gmail.com>
2018-08-26 07:12:07 -0500

    about to draw rectangles to construct image

--- lib/identicon.ex
+++ lib/identicon.ex
@@ -4,6 +4,8 @@ defmodule Identicon do
     |> hash_input
     |> pick_color
     |> build_grid
+    |> filter_odd_numbers
+    |> build_pixel_map
   end
 
   def pick_color(%Identicon.Image{ hex: [r, g, b | _tail] } = image) do
@@ -18,16 +20,40 @@ defmodule Identicon do
   end
 
   def build_grid(%Identicon.Image{ hex: hex_list } = image) do
-    hex_list
-    |> Enum.chunk(3)
-    |> mirrow_row
+    grid =
+      hex_list
+      |> Enum.chunk_every(3, 3, :discard)
+      |> Enum.map(&mirrow_row/1)
+      |> List.flatten
+      |> Enum.with_index
+    
+    %Identicon.Image{ image | grid: grid }
   end
   
-  def mirrow_row(lists) do
-    for list <- lists do
-      rev_list = Enum.reverse(list)
-      front = Enum.take(list, length(list) - 1)
-      Enum.concat(front, rev_list)
-    end
+  def mirrow_row(row) do
+    [first, second | _tail] = row
+    row ++ [second, first]
   end
+
+  def filter_odd_numbers(%Identicon.Image{ grid: grid } = image) do
+    grid = Enum.filter grid, fn({num, _idx}) ->
+      rem(num, 2) == 0 
+    end
+
+    %Identicon.Image{ image | grid: grid }
+  end
+
+  def build_pixel_map(%Identicon.Image{ grid: grid } = image) do
+    pixel_map = Enum.map grid, fn({_num, idx}) ->
+      horiz = rem(idx, 5) * 50
+      vert = div(idx, 5) * 50
+      top_left = {horiz, vert}
+      bottom_right = {horiz + 50, vert + 50}
+
+      {top_left, bottom_right}
+    end
+
+    %Identicon.Image{ image | pixel_map: pixel_map }
+  end
+
 end
