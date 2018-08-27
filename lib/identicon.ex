8793f62ef58c36918db472300b4ad1b8bffed0cd
austinChappell <Austin.W.Chappell@gmail.com>
2018-08-26 23:34:16 -0500

    add egd dep

--- lib/identicon.ex
+++ lib/identicon.ex
@@ -6,6 +6,8 @@ defmodule Identicon do
     |> build_grid
     |> filter_odd_numbers
     |> build_pixel_map
+    |> draw_image
+    |> save_image(input)
   end
 
   def pick_color(%Identicon.Image{ hex: [r, g, b | _tail] } = image) do
@@ -54,6 +56,21 @@ defmodule Identicon do
     end
 
     %Identicon.Image{ image | pixel_map: pixel_map }
+  end
+
+  def draw_image(%Identicon.Image{ color: color, pixel_map: pixel_map }) do
+    image = :egd.create(250, 250)
+    fill = :egd.color(color)
+
+    Enum.each pixel_map, fn({start, stop}) ->
+      :egd.filledRectangle(image, start, stop, fill)
+    end
+
+    :egd.render(image)
+  end
+
+  def save_image(image, filename) do
+    File.write("#{filename}.png", image)
   end
 
 end
